51d1ff4dd2344370ca66a178727c9afdf67736c2
Nadifo Ismail <nadifoismail80@gmail.com>
2025-01-26 20:10:49 +0300

    modifies app.py and models.py

--- server/app.py
+++ server/app.py
@@ -24,6 +24,61 @@ api = Api(app)
 def index():
     return "<h1>Code challenge</h1>"
 
+@app.route("/restaurants",methods =["GET"])
+def get_restaurants():
+    restaurants = Restaurant.query.all()
+    response = [restaurant.to_dict(only=("id", "name", "address")) for restaurant in restaurants]
+    return make_response(response),200
 
+
+@app.route("/restaurants/<int:id>", methods=["GET","DELETE"])
+def get_restaurant_by_id(id):
+    restaurant = db.session.get(Restaurant, id)
+    if request.method == "GET":
+        if restaurant:
+            return make_response(restaurant.to_dict()), 200
+        return make_response({"error": "Restaurant not found"}), 404
+    
+    if request.method == "DELETE":
+       if restaurant:
+        db.session.delete(restaurant)
+        db.session.commit()
+        return make_response ("", 204)
+    return make_response({"error": "Restaurant not found"}), 404
+
+@app.route("/pizzas", methods=["GET"])
+def get_pizzas():
+    pizzas = Pizza.query.all()
+    return make_response([pizza.to_dict(only=("id", "name", "ingredients")) for pizza in pizzas]), 200
+
+@app.route("/restaurant_pizzas", methods=["POST"])
+def create_restaurant_pizza():
+    data = request.get_json()
+
+    price = data.get("price")
+    pizza_id = data.get("pizza_id")
+    restaurant_id = data.get("restaurant_id")
+
+    if not (1 <= price <= 30):
+        return make_response({"errors": ["validation errors"]}), 400
+
+    try:
+        pizza = db.session.get(Pizza, pizza_id)  
+        restaurant = db.session.get(Restaurant, restaurant_id)
+
+        if not pizza or not restaurant:
+            return make_response({"errors": ["validation errors"]}), 400
+
+        new_restaurant_pizza = RestaurantPizza(price=price, pizza_id=pizza_id, restaurant_id=restaurant_id)
+        db.session.add(new_restaurant_pizza)
+        db.session.commit()
+
+        
+        response = new_restaurant_pizza.to_dict()
+        return make_response(response), 201
+
+    except Exception:
+        return make_response({"errors": ["validation errors"]}), 400
+    
 if __name__ == "__main__":
     app.run(port=5555, debug=True)
--- server/models.py
+++ server/models.py
@@ -21,8 +21,15 @@ class Restaurant(db.Model, SerializerMixin):
     address = db.Column(db.String)
 
     # add relationship
+    restaurant_pizzas = db.relationship('RestaurantPizza',back_populates='restaurant',cascade ='all,delete-orphan')
+
+    # Association proxy for pizzas through RestaurantPizza
+    pizzas = association_proxy("restaurant_pizzas", "pizza")
+    
 
     # add serialization rules
+    serialize_rules = ("-restaurant_pizzas.restaurant",)
+
 
     def __repr__(self):
         return f"<Restaurant {self.name}>"
@@ -36,8 +43,14 @@ class Pizza(db.Model, SerializerMixin):
     ingredients = db.Column(db.String)
 
     # add relationship
+    restaurant_pizzas = db.relationship('RestaurantPizza',back_populates='pizza',cascade ='all,delete-orphan')
+
+    # Association proxy for restaurants through RestaurantPizza
+    restaurants = association_proxy("restaurant_pizzas", "restaurant")
 
     # add serialization rules
+    serialize_rules =("-restaurant_pizzas.pizza",)
+
 
     def __repr__(self):
         return f"<Pizza {self.name}, {self.ingredients}>"
@@ -49,11 +62,23 @@ class RestaurantPizza(db.Model, SerializerMixin):
     id = db.Column(db.Integer, primary_key=True)
     price = db.Column(db.Integer, nullable=False)
 
+    restaurant_id = db.Column(db.Integer, db.ForeignKey("restaurants.id"))
+    pizza_id = db.Column(db.Integer, db.ForeignKey("pizzas.id"))
+
     # add relationships
+    restaurant = db.relationship('Restaurant',back_populates='restaurant_pizzas')
+
+    pizza = db.relationship('Pizza', back_populates='restaurant_pizzas')
 
     # add serialization rules
+    serialize_rules = ("-restaurant.restaurant_pizzas", "-pizza.restaurant_pizzas")
 
     # add validation
+    @validates('price')
+    def validates(self,key,price):
+        if not (1 <= price <= 30):
+            raise ValueError("Price must be between 1 and 30")
+        return price
 
     def __repr__(self):
         return f"<RestaurantPizza ${self.price}>"
